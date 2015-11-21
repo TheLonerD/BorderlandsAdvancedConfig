acef59c308d33333a62866fda87dcfc429e90fb7
Lightwave <thelonerd@gmail.com>
2015-11-21 17:17:02 +0300

    Disable maximize box for main form.

--- FrmConfig.Designer.cs
+++ FrmConfig.Designer.cs
@@ -576,6 +576,7 @@
             this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "FrmConfig";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Borderlands Advanced Settings";
